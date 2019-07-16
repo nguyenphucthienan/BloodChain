8671f9d6026a6eca5853fe18a9256762e5157853
Nguyen Phuc Thien An <npta97@gmail.com>
2019-07-16 14:26:09 +0700

    Update ManagerBloodPackManager component

--- src/app/manager/components/manager-blood-pack-manager/manager-blood-pack-manager.component.ts
+++ src/app/manager/components/manager-blood-pack-manager/manager-blood-pack-manager.component.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { MDBModalRef } from 'angular-bootstrap-md';
+import { Router } from '@angular/router';
+import { MDBModalRef, MDBModalService } from 'angular-bootstrap-md';
 import { fromEvent, Subscription } from 'rxjs';
 import { debounceTime, map, tap } from 'rxjs/operators';
 import { DatatableComponent } from 'src/app/datatable/datatable.component';
 import { TableActionType } from 'src/app/datatable/models/table-action.interface';
 import { TableCellChange } from 'src/app/datatable/models/table-cell-change.interface';
+import { ScanQrcodeModalComponent } from 'src/app/shared/modals/scan-qrcode-modal/scan-qrcode-modal.component';
 
 import { BloodPackManagerLiteTableService } from '../../services/blood-pack-manager-lite-table.service';
 
@@ -24,7 +26,9 @@ export class ManagerBloodPackManagerComponent implements OnInit, AfterViewInit, 
 
   constructor(
     public bloodPackManagerLiteTableService: BloodPackManagerLiteTableService,
-    private renderer: Renderer2
+    private router: Router,
+    private renderer: Renderer2,
+    private modalService: MDBModalService
   ) { }
 
   ngOnInit() {
@@ -42,11 +46,32 @@ export class ManagerBloodPackManagerComponent implements OnInit, AfterViewInit, 
   }
 
   searchBloodPack(value: string) {
-    if (value.length === 0 || value.length > 2) {
+    if (value.length === 0 || value.length === 24) {
       this.bloodPackManagerLiteTableService.pagination.page = 1;
       this.bloodPackManagerLiteTableService.filterMode._id = value;
       this.datatable.refresh();
     }
+  }
+
+  openScanQrCodeModal() {
+    this.modalRef = this.modalService.show(ScanQrcodeModalComponent, {
+      backdrop: true,
+      keyboard: true,
+      focus: true,
+      show: false,
+      ignoreBackdropClick: true,
+      class: 'modal-lg modal-dialog-centered',
+      containerClass: 'top',
+      animated: true
+    });
+
+    this.modalRef.content.scanSuccess
+      .subscribe((bloodPackId: string) => this.onQrCodeScanSuccess(bloodPackId));
+  }
+
+  onQrCodeScanSuccess(bloodPackId: string) {
+    this.bloodPackManagerLiteTableService.filterMode._id = bloodPackId;
+    this.datatable.refresh();
   }
 
   onTableCellChanged(tableCellChange: TableCellChange) {
@@ -59,6 +84,7 @@ export class ManagerBloodPackManagerComponent implements OnInit, AfterViewInit, 
   }
 
   navigateToBloodPackDetail(id: string) {
+    this.router.navigate(['/manager', 'blood-packs', id]);
   }
 
   ngOnDestroy() {
