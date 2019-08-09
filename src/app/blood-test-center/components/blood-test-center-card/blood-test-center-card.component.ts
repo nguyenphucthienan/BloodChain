bad079e826f9d30dad5a99e3723f7d492f950ad3
Nguyen Phuc Thien An <npta97@gmail.com>
2019-08-09 22:56:46 +0700

    Update BloodTestCenterCard component

--- src/app/blood-test-center/components/blood-test-center-card/blood-test-center-card.component.ts
+++ src/app/blood-test-center/components/blood-test-center-card/blood-test-center-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from 'ngx-gallery';
 import { BloodTestCenter } from 'src/app/core/models/blood-test-center.interface';
 
 @Component({
@@ -10,9 +11,40 @@ export class BloodTestCenterCardComponent implements OnInit {
 
   @Input() bloodTestCenter: BloodTestCenter;
 
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '100%',
+        preview: false,
+        thumbnails: false,
+        imageAutoPlay: true,
+        imageAutoPlayInterval: 3000,
+        imageAutoPlayPauseOnHover: true,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        imageInfinityMove: true,
+        imageArrows: false
+      },
+      {
+        breakpoint: 576,
+        width: '100%',
+        height: '200px',
+      }
+    ];
+
+    this.galleryImages = this.bloodTestCenter.photos.map(photo => {
+      return {
+        id: photo._id,
+        small: photo.secureUrl,
+        medium: photo.secureUrl,
+        big: photo.secureUrl
+      };
+    });
   }
 
 }
