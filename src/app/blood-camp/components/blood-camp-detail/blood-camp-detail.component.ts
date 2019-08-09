b9f5197dfdd68dfc936c53e39b15e574a7d3bcf0
Nguyen Phuc Thien An <npta97@gmail.com>
2019-08-09 22:17:06 +0700

    Update BloodCampDetail component

--- src/app/blood-camp/components/blood-camp-detail/blood-camp-detail.component.ts
+++ src/app/blood-camp/components/blood-camp-detail/blood-camp-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from 'ngx-gallery';
 import { BloodCamp } from 'src/app/core/models/blood-camp.interface';
+import { Point } from 'src/app/core/models/point.interface';
 
 @Component({
   selector: 'app-blood-camp-detail',
@@ -10,6 +12,10 @@ import { BloodCamp } from 'src/app/core/models/blood-camp.interface';
 export class BloodCampDetailComponent implements OnInit, OnDestroy {
 
   bloodCamp: BloodCamp;
+  point: Point;
+
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -18,8 +24,31 @@ export class BloodCampDetailComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.renderer.addClass(document.body, 'grey-background');
+    this.galleryOptions = [{
+      width: '100%',
+      height: '600px',
+      thumbnailsColumns: 5,
+      thumbnailsArrowsAutoHide: true,
+      imageAnimation: NgxGalleryAnimation.Slide,
+      imageInfinityMove: true,
+      imageArrowsAutoHide: true,
+      previewInfinityMove: true,
+      previewZoom: true,
+      previewFullscreen: true,
+      previewCloseOnEsc: true,
+    }];
+
     this.route.data.subscribe((data: any) => {
       this.bloodCamp = data.bloodCamp;
+      this.point = this.bloodCamp.location;
+      this.galleryImages = this.bloodCamp.photos.map(photo => {
+        return {
+          id: photo._id,
+          small: photo.secureUrl,
+          medium: photo.secureUrl,
+          big: photo.secureUrl
+        };
+      });
     });
   }
 
